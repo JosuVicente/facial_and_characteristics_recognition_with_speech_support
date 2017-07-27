93ceb4021f51669deeb1dd3b04c66dab3c967fc3
JosuVicente <josuvicente@gmail.com>
2017-07-27 11:20:51 +0100

    Update documentation

--- src/speech_utils.py
+++ src/speech_utils.py
@@ -4,7 +4,7 @@ import io
 import os
 
 #######################
-GOOGLE_CLOUD_SPEECH_CREDENTIALS_PATH = '../files/TFM project-287dc6d9869a.json'
+GOOGLE_CLOUD_SPEECH_CREDENTIALS_PATH = '../files/GoogleCloudKey.json'
 #######################
 
 def transcript_audio(filepath, language, use_cloud):
